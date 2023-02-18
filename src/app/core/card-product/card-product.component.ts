e861cde1bc90c3c7027539095cf7c0f8ea0e984d
Matheus-BezerraaVanguard <matheus.bezerra@sistemavangurd.com.br>
2023-02-18 16:15:59 -0300

    Componente de card no projeto

---
+++ src/app/core/card-product/card-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mg-card-product',
+  templateUrl: './card-product.component.html',
+  styleUrls: ['./card-product.component.scss']
+})
+export class CardProductComponent {
+
+}
